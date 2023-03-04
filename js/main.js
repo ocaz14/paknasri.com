d20b9cad6ed66e2587f70435878f61537a35a91b
ocaz14 <nasri.anas14@gmail.com>
2023-03-04 15:15:52 +0800

    udpate html css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function dropdown_open(){
+    document.getElementById("dropdown-menu").style.visibility = "visible";
+    document.getElementById("dropdown_botton").style.visibility = "hidden";
+    document.getElementById("dropdown_close").style.visibility = "visible";
+}
+function dropdown_close(){
+    document.getElementById("dropdown-menu").style.visibility = "hidden";
+    document.getElementById("dropdown_botton").style.visibility = "visible";
+    document.getElementById("dropdown_close").style.visibility = "hidden";
+}
