67e81cd2c6b23fdb4eb6fff022559f23d6574148
ocaz14 <nasri.anas14@gmail.com>
2023-05-20 12:49:15 +0800

    update link

--- js/main.js
+++ js/main.js
@@ -8,3 +8,36 @@ function dropdown_close(){
     document.getElementById("dropdown_botton").style.visibility = "visible";
     document.getElementById("dropdown_close").style.visibility = "hidden";
 }
+
+var dropdown_menu_id = document.getElementById("dropdown-menu")
+var side_menu = 
+"<div class='cont-head'>"+
+"<div class='logo'>"+
+"    pakNasri.com"+
+"</div>"+
+"<div class='nav'>"+
+"    <a href='/index'><div class='nav-child'>HOME</div></a>"+
+"    <a href='/blog'><div class='nav-child'>BLOG</div></a>"+
+"</div>"+
+"</div>";  
+dropdown_menu_id.innerHTML = side_menu;
+
+var menu_button_id = document.getElementById("menu_button")
+var menu_button = 
+"<div onclick='dropdown_open()' class='dropdown-botton' id='dropdown_botton'>"+
+"   <div class='dropdown-botton-child'></div>"+
+"   <div class='dropdown-botton-child'></div>"+
+"   <div class='dropdown-botton-child'></div>"+
+"</div>"+
+"<div onclick='dropdown_close()' class='dropdown-close' id='dropdown_close'>"+
+"   &#x2716;"+
+"</div>";
+menu_button_id.innerHTML = menu_button;
+
+
+
+
+
+
+
+
