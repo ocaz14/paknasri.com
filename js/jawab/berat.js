c778863126eb5e1d8186fe70a449b35768204a38
ocaz14 <nasri.anas14@gmail.com>
2023-09-04 21:06:57 +0800

    update berat

---
+++ js/jawab/berat.js
@@ -0,0 +1,9 @@
+var url_root = "https://paknasri.com";
+var list_arr = [
+    //id, name, url
+    ["berat_001", "/jawab/berat/100-gram-berapa-ons", "Cara menghitung 100 gram berapa ons dengan mudah"],
+    ["berat_002", "/jawab/berat/1-gram-berapa-ons", "Cara menghitung 1 gram berapa ons dengan mudah"],
+    ["berat_003", "/jawab/berat/10-ons-berapa-kg", "Cara menghitung 10 ons berapa kg dengan mudah"],
+    ["berat_004", "/jawab/berat/1000-gram-berapa-kg", "Cara menghitung 1000 gram berapa kg dengan mudah"],
+    
+]
